0e69f2c9294b9d0f25bf6b61af601273f320b55a
harry <sksskdf@github.com>
2023-01-30 15:00:33 +0900

    2023-01-30

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Harry";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "HARRY'S BLOG";
 
 export default function Layout({ children, home }) {
   return (
